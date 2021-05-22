77429eadd1f8d5c1c5ebcabf762bab349a9da373
Tammy Lam <lamtammy1515@gmail.com>
2021-05-22 12:38:01 -0500

    Fixed unknown error

---
+++ db/migrate/20210522165142_remove_meal_id.rb
@@ -0,0 +1,5 @@
+class RemoveMealId < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :comments, :meal_id, :integer 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_043245) do
+ActiveRecord::Schema.define(version: 2021_05_22_165142) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
